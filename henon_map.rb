d8b219a273b28be0b870237f1ec83a1ba1bcabb5
Jacob Perkins <jacob.a.perkins@gmail.com>
2010-08-18 00:38:07 -0500

    making things way cleaner, playing with gnuplot

--- henon_map.rb
+++ henon_map.rb
@@ -1,18 +1,26 @@
 #!/usr/bin/env ruby
 
-def henon_map
-  steps = 10000
-  a = 1.4
-  b = 0.3
-  x = 0.631354477
-  y = 0.189406343
-  
-  steps.times do
-    x_old = x
-    x = y + 1 - a*x*x
-    y = b*x_old
-    puts [x,y].join("\t") + "\n"
-  end
+require 'rubygems'
+require 'gnuplot'
+
+#
+# Generate points from the henon map starting at
+# coordinates (x,y) and stepping forward n iterations
+#
+def henon_map n, x, y, &blk
+  a, b = [1.4, 0.3] # parameters defining how the map behaves
+  n.times{x_n = x; yield [x = y + 1 - a*x*x, y = b*x_n]}
 end
 
-henon_map
+#
+# Boring plotting routine
+#
+Gnuplot.open do |gp|
+  Gnuplot::Plot.new(gp) do |plot|
+    map = []
+    henon_map(10000, 0.631354477, 0.189406343){|pt| map << pt}
+    plot.data << Gnuplot::DataSet.new([map.transpose.first, map.transpose.last]) do |ds|
+      ds.with  = "points"
+    end
+  end
+end
